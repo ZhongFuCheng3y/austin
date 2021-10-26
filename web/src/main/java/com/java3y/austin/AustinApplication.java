df703f126526264d804df439c24dcd0870e88309
三歪 <sanwai@mogu.com>
2021-10-26 21:17:31 +0800

    austin start

---
+++ web/src/main/java/com/java3y/austin/AustinApplication.java
@@ -0,0 +1,23 @@
+package com.java3y.austin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class AustinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AustinApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
