c3620c125ec8ccf940a4b095cf7da23771a319ec
三歪 <sanwai@mogu.com>
2021-10-30 15:18:25 +0800

    add logback

--- austin-web/src/main/java/com/java3y/austin/AustinApplication.java
+++ austin-web/src/main/java/com/java3y/austin/AustinApplication.java
@@ -1,5 +1,7 @@
 package com.java3y.austin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AustinApplication {
 
+    private final Logger logger = LoggerFactory.getLogger(AustinApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AustinApplication.class, args);
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.error("error logback for austin");
+        logger.info("info logback for austin");
         return String.format("Hello %s!", name);
     }
 
