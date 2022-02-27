be7efd485a9b45d5d852b01df310fb6ec06ef14f
whywhathow <whywhathow.fun@gmail.com>
2022-02-27 13:56:42 +0800

    feat(Spring线程池优化->yaml文档配置):
    默认配置属性类: AsyncExecutionProperties
    yaml配置文档:
    austin-web/src/main/resources/application.yml

--- austin-cron/src/main/java/com/java3y/austin/cron/config/AsyncConfiguration.java
+++ austin-cron/src/main/java/com/java3y/austin/cron/config/AsyncConfiguration.java
@@ -3,43 +3,46 @@ package com.java3y.austin.cron.config;
 import com.google.common.base.Throwables;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
-
 /**
  * 处理定时任务的线程池配置信息，为@Async注解服务
+ * 自定义线程池配置
  *
  * @author 3y
+ * @see TaskExecutionAutoConfiguration
  */
 @Slf4j
 @Configuration
 @EnableAsync
+@EnableConfigurationProperties(AsyncExecutionProperties.class)
 public class AsyncConfiguration implements AsyncConfigurer {
-
     @Bean("austinExecutor")
-    public ThreadPoolTaskExecutor executor() {
+    @Primary
+    public ThreadPoolTaskExecutor executor(AsyncExecutionProperties properties) {
+        log.info("funExecutor -- init ");
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(30);
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        executor.setThreadNamePrefix("austinAsyncExecutor-");
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.setAwaitTerminationSeconds(10);
+        executor.setCorePoolSize(properties.getCoreSize()); // 核心线程数
+        executor.setMaxPoolSize(properties.getMaxSize()); // 最大线程数
+        executor.setKeepAliveSeconds(properties.getKeepAlive()); // 最大存活时间
+        executor.setQueueCapacity(properties.getQueueCapacity()); // 阻塞队列容量
+        executor.setThreadNamePrefix(properties.getThreadNamePrefix()); // 设置名称前缀
+        executor.setRejectedExecutionHandler(properties.getRejectedHandler().getHandler());// 设置拒绝策略
+        executor.setAllowCoreThreadTimeOut(properties.isAllowCoreThreadTimeout());// 是否允许核心线程超时
+        executor.setWaitForTasksToCompleteOnShutdown(properties.isWaitForTasksToCompleteOnShutDown());
+        executor.setAwaitTerminationSeconds(properties.getAwaitTerminationSeconds());
+        log.info("austinExecutor: {} ", executor);
         executor.initialize();
         return executor;
     }
 
-    @Override
-    public Executor getAsyncExecutor() {
-        return executor();
-    }
 
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
