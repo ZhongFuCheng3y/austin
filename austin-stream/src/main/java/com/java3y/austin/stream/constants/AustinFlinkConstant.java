0099f9c1b781082348082390360e6bec0ce4a6c4
3y <zhongfucheng_123@163.com>
2022-07-07 20:42:13 +0800

    add comment

--- austin-stream/src/main/java/com/java3y/austin/stream/constants/AustinFlinkConstant.java
+++ austin-stream/src/main/java/com/java3y/austin/stream/constants/AustinFlinkConstant.java
@@ -10,6 +10,7 @@ public class AustinFlinkConstant {
      * Kafka 配置信息
      * TODO 使用前配置kafka broker ip:port
      * (真实网络ip,这里不能用配置的hosts，看语雀文档得到真实ip)
+     * （如果想要自己监听到所有的消息，改掉groupId）
      */
     public static final String GROUP_ID = "austinLogGroup";
     public static final String TOPIC_NAME = "austinTraceLog";
