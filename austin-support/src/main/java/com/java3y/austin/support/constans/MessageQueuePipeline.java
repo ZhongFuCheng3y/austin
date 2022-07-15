f6b6e23866a1c2474fa64123ce4556117ae34fe9
xzcawl <awlxia@tencent.com>
2022-07-15 17:36:28 +0800

    add rabbitMq send

--- austin-support/src/main/java/com/java3y/austin/support/constans/MessageQueuePipeline.java
+++ austin-support/src/main/java/com/java3y/austin/support/constans/MessageQueuePipeline.java
@@ -10,5 +10,6 @@ public interface MessageQueuePipeline {
     String EVENT_BUS = "eventBus";
     String KAFKA = "kafka";
     String ROCKET_MQ = "rocketMq";
+    String RABBIT_MQ = "rabbitMq";
 
 }
