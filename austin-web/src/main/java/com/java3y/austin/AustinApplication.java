6ba128af2912aab66113f3015a7ab2c10ab40566
3y <zhongfucheng_123@163.com>
2022-08-05 22:04:02 +0800

    update comment

--- austin-web/src/main/java/com/java3y/austin/AustinApplication.java
+++ austin-web/src/main/java/com/java3y/austin/AustinApplication.java
@@ -11,7 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AustinApplication {
     public static void main(String[] args) {
 
-        // TODO 如果你需要使用apollo，将application.properties配置文件的 apollo.enabled 改为true
+        /**
+         * 如果你需要启动Apollo动态配置
+         * 1、启动apollo
+         * 2、将application.properties配置文件的 austin.apollo.enabled 改为true
+         * 3、下方的property替换真实的ip和port
+         */
         System.setProperty("apollo.config-service", "http://austin.apollo.config:5001");
         SpringApplication.run(AustinApplication.class, args);
     }
