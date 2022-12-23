d2ef1b1a4f87f05dad1c468a8a97df3bee82bd2b
3y <zhongfucheng_123@163.com>
2022-12-23 18:58:47 +0800

    add stream-start.sh

---
+++ stream-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker cp ./austin-stream/target/austin-stream-0.0.1-SNAPSHOT.jar austin_jobmanager_1:/opt/
+
+docker exec -ti austin_jobmanager_1 bash
+
+flink run /opt/austin-stream-0.0.1-SNAPSHOT.jar
