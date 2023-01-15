36b9fe60321d28cddd12d2185af089efd902f549
3y <zhongfucheng_123@163.com>
2023-01-15 20:53:22 +0800

    微信测试账号删除注册用户接口

--- austin-common/src/main/java/com/java3y/austin/common/constant/OfficialAccountParamConstant.java
+++ austin-common/src/main/java/com/java3y/austin/common/constant/OfficialAccountParamConstant.java
@@ -39,7 +39,7 @@ public class OfficialAccountParamConstant {
     /**
      * 扫码/关注后/取消关注后的服务号文案
      */
-    public static final String SUBSCRIBE_TIPS = "关注公众号：Java3y 回复 austin 可获取项目笔记哟！";
+    public static final String SUBSCRIBE_TIPS = "项目群还有少量名额，添加我的微信 sanwai3y  备注【项目】，我会拉入项目群";
     public static final String SCAN_TIPS = "咋又扫码啦？重新关注一波吧！";
     public static final String UNSUBSCRIBE_TIPS = "老乡别走！";
 
