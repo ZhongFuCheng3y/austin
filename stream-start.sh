79ac11fdb3170b145e90367820d88e08e007a5fd
3y <zhongfucheng_123@163.com>
2023-01-28 17:33:29 +0800

    更改 默认的docker-compose 配置

--- stream-start.sh
+++ stream-start.sh
@@ -3,7 +3,11 @@
 docker cp ./austin-stream/target/austin-stream-0.0.1-SNAPSHOT.jar austin_jobmanager_1:/opt/
 docker exec -ti austin_jobmanager_1 flink run /opt/austin-stream-0.0.1-SNAPSHOT.jar
 
-# local test
-# docker cp ./austin-stream-0.0.1-SNAPSHOT.jar flink_docker_jobmanager_1:/opt/
-# docker exec -ti flink_docker_jobmanager_1 flink run /opt/austin-stream-0.0.1-SNAPSHOT.jar
+# stream local test
+# docker cp ./austin-stream-0.0.1-SNAPSHOT.jar austin_jobmanager_1:/opt/austin-stream-test-0.0.1-SNAPSHOT.jar
+# docker exec -ti austin_jobmanager_1 flink run /opt/austin-stream-test-0.0.1-SNAPSHOT.jar
+
+
+# data-house
+# ./flink run austin-data-house-0.0.1-SNAPSHOT.jar
 
