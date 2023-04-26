6b26e3959baa5cffc93000e2ed0571e483a04ce1
3y <zhongfucheng_123@163.com>
2023-04-26 21:19:06 +0800

    fix bug:fix case exception

--- austin-web/src/main/java/com/java3y/austin/web/controller/RefreshTokenController.java
+++ austin-web/src/main/java/com/java3y/austin/web/controller/RefreshTokenController.java
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 /**
- * @Author 3y
+ * @author 3y
  */
-@AustinResult
+
 @AustinAspect
 @Api(tags = {"手动刷新token的接口"})
 @RestController
