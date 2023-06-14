2f333c8aa9a4fb49fb2690520dce71a846c01c99
sky.huang <sky.huang@1data.info>
2023-06-14 11:48:27 +0800

    Fixes 哨兵模式未设置端口

--- austin-stream/src/main/java/com/java3y/austin/stream/utils/LettuceRedisUtils.java
+++ austin-stream/src/main/java/com/java3y/austin/stream/utils/LettuceRedisUtils.java
@@ -29,12 +29,12 @@ public class LettuceRedisUtils {
     static {
         RedisURI redisUri = null;
         if (StrUtil.equals(AustinFlinkConstant.REDIS_MODE_SENTINEL, AustinFlinkConstant.REDIS_MODE)) {
-            redisUri = RedisURI.Builder.sentinel(AustinFlinkConstant.REDIS_IP, AustinFlinkConstant.MASTER_ID)
+            redisUri = RedisURI.Builder.sentinel(AustinFlinkConstant.REDIS_IP, AustinFlinkConstant.REDIS_PORT, AustinFlinkConstant.MASTER_ID)
                     .withPassword(AustinFlinkConstant.REDIS_PASSWORD.toCharArray())
                     .build();
         } else if (StrUtil.equals(AustinFlinkConstant.REDIS_MODE_SINGLE, AustinFlinkConstant.REDIS_MODE)) {
             redisUri = RedisURI.Builder.redis(AustinFlinkConstant.REDIS_IP)
-                    .withPort(Integer.valueOf(AustinFlinkConstant.REDIS_PORT))
+                    .withPort(AustinFlinkConstant.REDIS_PORT)
                     .withPassword(AustinFlinkConstant.REDIS_PASSWORD.toCharArray())
                     .build();
         }
