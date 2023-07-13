16e81f367b6f506de98e9b12b30bd80cfac8e343
sky.huang <sky.huang@1data.info>
2023-07-13 09:35:14 +0800

    提供 xxl-job 注册 address 配置, 便于容器内注册到 外部xxl-job 场景下, 调用通畅

--- austin-cron/src/main/java/com/java3y/austin/cron/xxl/config/XxlJobConfig.java
+++ austin-cron/src/main/java/com/java3y/austin/cron/xxl/config/XxlJobConfig.java
@@ -20,6 +20,8 @@ public class XxlJobConfig {
     private String adminAddresses;
     @Value("${xxl.job.executor.appname}")
     private String appName;
+    @Value("${xxl.job.executor.address}")
+    private String address;
     @Value("${xxl.job.executor.ip}")
     private String ip;
     @Value("${xxl.job.executor.port}")
@@ -37,6 +39,7 @@ public class XxlJobConfig {
         XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
         xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
         xxlJobSpringExecutor.setAppname(appName);
+        xxlJobSpringExecutor.setAddress(address);
         xxlJobSpringExecutor.setIp(ip);
         xxlJobSpringExecutor.setPort(port);
         xxlJobSpringExecutor.setAccessToken(accessToken);
