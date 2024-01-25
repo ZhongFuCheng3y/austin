baa6ae094e818c1b37347c184add61611a51091d
diaobisong <bitdbsd11@163.com>
2024-01-25 18:24:13 +0800

    fix code

--- austin-web/src/main/java/com/java3y/austin/web/interceptor/TokenInterceptor.java
+++ austin-web/src/main/java/com/java3y/austin/web/interceptor/TokenInterceptor.java
@@ -26,6 +26,10 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+
         if(request.getRequestURI().startsWith("/actuator") || request.getRequestURI().startsWith("/swagger") || request.getRequestURI().startsWith("/v3/api-docs") || request.getRequestURI().startsWith("/user/login")) {
             return true;
         }
