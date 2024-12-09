6605316a53d59b7af1bdf2933d37aafdcc9b5786
larry <945645265@qq.com>
2024-12-10 00:11:04 +0800

    feat:增强rabbitmq生产者消息可靠性，添加publisher-confirm和publisher-return

--- austin-support/src/main/java/com/java3y/austin/support/mq/rabbit/RabbitSendMqServiceImpl.java
+++ austin-support/src/main/java/com/java3y/austin/support/mq/rabbit/RabbitSendMqServiceImpl.java
@@ -1,8 +1,11 @@
 package com.java3y.austin.support.mq.rabbit;
 
+import cn.hutool.core.util.IdUtil;
+import com.google.common.base.Throwables;
 import com.java3y.austin.support.constans.MessageQueuePipeline;
 import com.java3y.austin.support.mq.SendMqService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -40,10 +43,20 @@ public class RabbitSendMqServiceImpl implements SendMqService {
 
     @Override
     public void send(String topic, String jsonValue, String tagId) {
+        CorrelationData correlationData = new CorrelationData(IdUtil.getSnowflake().nextIdStr());
+        correlationData.getFuture().addCallback(result -> {
+            if (result.isAck()) {
+                log.info("消息成功投递到交换机，消息ID：{}", correlationData.getId());
+            }else{
+                log.error("消息投递到交换机失败，消息ID：{}", correlationData.getId());
+            }
+        }, ex -> {
+            log.error("消息处理异常，{}", Throwables.getStackTraceAsString(ex));
+        });
         if (topic.equals(sendMessageTopic)){
-            rabbitTemplate.convertAndSend(exchangeName, sendRoutingKey, jsonValue);
+            rabbitTemplate.convertAndSend(exchangeName, sendRoutingKey, jsonValue, correlationData);
         }else if (topic.equals(austinRecall)){
-            rabbitTemplate.convertAndSend(exchangeName, recallRoutingKey, jsonValue);
+            rabbitTemplate.convertAndSend(exchangeName, recallRoutingKey, jsonValue, correlationData);
         }else {
             log.error("RabbitSendMqServiceImpl send topic error! topic:{}", topic);
         }
